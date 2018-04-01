a89c2b97f0d709116594ba8748fd9dcd84e55a26
Werner Koch <werner.koch@bluewin.ch>
2018-04-01 21:52:57 +0200

    (kow) Whatever.

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,27 @@
+import webdriver = require('webdriverio');
+
 export class Index {
     public static main() {
         console.log('Hello world!');
+        Index.doIt();
+    }
+
+    private static doIt() {
+        webdriver
+            .remote({
+                desiredCapabilities: {
+                    browserName: 'firefox'
+                }
+            })
+            .init()
+            .url('http://www.google.com')
+            .getTitle()
+            .then((title) => {
+                console.log('Title was: ' + title);
+            }).end()
+            .catch((err) => {
+                console.log(err);
+            });
     }
 }
 Index.main();
