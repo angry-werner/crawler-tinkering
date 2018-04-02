c2898c2d8bd07d62ceed1cb4a5889327b60c87d6
Werner Koch <werner.koch@bluewin.ch>
2018-04-02 18:33:31 +0200

    (kow) One date, the links.

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 Tinkering with a crawler to help out in another project
 
 ## Install Selenium
-$mkdir selenium && cd selenium
-
-curl -O http://selenium-release.storage.googleapis.com/3.11/selenium-server-standalone-3.11.0.jar
-
-curl -L https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz | tar xz
-
-nohup java -jar -Dwebdriver.gecko.driver=./geckodriver selenium-server-standalone-3.11.0.jar &
-
-Process ID aufschreiben
+npm install selenium-standalone // @latest -g
+selenium-standalone install
+selenium-standalone start
 
--- src/Index.ts
+++ src/Index.ts
@@ -2,26 +2,40 @@ import webdriver = require('webdriverio');
 
 export class Index {
     public static main() {
-        console.log('Hello world!');
-        Index.doIt();
+        console.log('Start crawling!');
+        Index.doIt().then((result) => {
+            if (result !== undefined) {
+                console.log('Result from top promise: ' + result);
+            }
+        }).catch((error) => {
+            console.log('Error from top promise: ' + error);
+        });
+        console.log('End crawling!');
     }
 
-    private static doIt() {
-        webdriver
-            .remote({
-                desiredCapabilities: {
-                    browserName: 'chrome'
-                }
-            })
-            .init()
-            .url('https://www.blickamabend.ch')
-            .getTitle()
-            .then((title) => {
-                console.log('Title was: ' + title);
-            }).end()
-            .catch((err) => {
-                console.log(err);
-            });
+    private static async doIt() {
+        const browser: WebdriverIO.Client<void> = webdriver.remote({
+            desiredCapabilities: {
+                browserName: 'chrome'
+            }
+        });
+        try {
+            const session: WebdriverIO.Client<any> =
+                browser.init().url('https://www.blickamabend.ch/suche/?q=single 2903');
+            const elements: any = await session.elements('div.list > div.item');
+            console.log('Elemente: ' + elements.value.length);
+            for (const element of elements.value) {
+                const time: string = await session.elementIdElement(element.ELEMENT, 'span.time').getText();
+                const linkElement: any = await session.elementIdElement(element.ELEMENT, 'a.news-links');
+                const href: any = await session.elementIdAttribute(linkElement.value.ELEMENT, 'href');
+                console.log('Time: ' + time + ', href: ' + href.value);
+            }
+        } finally {
+            if (browser != null) {
+                // browser.close();
+            }
+        }
     }
 }
+
 Index.main();
